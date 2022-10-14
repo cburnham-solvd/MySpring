f9bf8bedf8dd1e13c65b5d676e13e5c177a4bf7e
cburnham-solvd <cburnham@solvd.com>
2022-10-14 08:26:09 -0500

    tables and data created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee values(10001,'James', 'Bond', '1980-06-01');
+insert into employee values(10002,'Black', 'Widow', '1981-06-01');
+insert into employee values(10003,'Dr', 'Strange', '1979-06-02');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table employee
+(
+    id integer not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    dob date not null,
+    primary key(id)
+);
